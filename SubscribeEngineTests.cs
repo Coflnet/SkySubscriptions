34200cd4152a4b1912ad87f374276ea93962971b
Äkwav <xekwavx@gmail.com>
2022-02-19 00:57:46 +0100

    use correct method

--- SubscribeEngineTests.cs
+++ SubscribeEngineTests.cs
@@ -10,7 +10,7 @@ namespace Coflnet.Sky.Subscriptions
         public void BasicMatch()
         {
             var notifi = new Mock<INotificationService>();
-            notifi.Setup(n => n.PriceAlert(It.IsAny<Subscription>(), "ASPECT", 5));
+            //notifi.Setup(n => n.AuctionPriceAlert(It.IsAny<Subscription>(), "ASPECT", 5));
             var engine = new SubscribeEngine(null, notifi.Object, null, null);
             var sub = new Subscription()
             {
@@ -18,16 +18,17 @@ namespace Coflnet.Sky.Subscriptions
                 Type = Subscription.SubType.BIN | Subscription.SubType.PRICE_HIGHER_THAN,
                 TopicId = "ASPECT"
             };
-            engine.AddNew(sub);
-            engine.NewAuction(new hypixel.SaveAuction()
+            var auction = new hypixel.SaveAuction()
             {
                 StartingBid = 5,
                 Tag = "ASPECT",
                 Start = System.DateTime.Now,
                 AuctioneerId = "",
                 Bin = true
-            });
-            notifi.Verify(n => n.PriceAlert(sub, "ASPECT", 5), Times.Once);
+            };
+            engine.AddNew(sub);
+            engine.NewAuction(auction);
+            notifi.Verify(n => n.AuctionPriceAlert(sub, auction), Times.Once);
         }
     }
 }
